7449e7e9c28cf6b2b8fbd8992c308c8992036f5b
Darron Froese <darron@froese.org>
2014-06-29 00:00:41 -0600

    Getting close.

---
+++ lib/services.rb
@@ -0,0 +1,26 @@
+class Services
+  def initialize()
+    @data = Hash.new { |hash, key| hash[key] = [] }
+  end
+  def [](key)
+    @data[key]
+  end
+  def []=(key,urls)
+    @data[key] += [urls].flatten
+    @data[key].uniq!
+  end
+  def get(command, service_type=http)
+    stdout_str, stderr_str, status = Open3.capture3(command)
+    stdout_str.each_line do |line|
+      service = JSON.parse(line)
+      unless service['ServiceTags'].nil?
+        if service['ServiceTags'].include? service_type
+          name = service['ServiceName']
+          url = "http://#{service['Address']}:#{service['ServicePort']}"
+          # urls[name] = %W( #{url} )
+          puts "#{name} at #{url}"
+        end
+      end
+    end
+  end
+end
--- octoconfig.gemspec
+++ octoconfig.gemspec
@@ -20,4 +20,5 @@ spec = Gem::Specification.new do |s|
   s.add_development_dependency('rdoc')
   s.add_development_dependency('aruba')
   s.add_runtime_dependency('gli','2.11.0')
+  s.add_runtime_dependency('json')
 end
